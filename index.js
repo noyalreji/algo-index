0d7b3a4bb6e782808fb012884feb4c49c654b0f9
Noyal Jacob <johndoe@example.comNoyal>
2021-08-14 22:00:13 -0500

    integers showing the cumulative sum at each index

---
+++ index.js
@@ -0,0 +1,13 @@
+// Return an array showing the cumulative sum at each index of an array of integers
+
+// Return: array — integers showing the cumulative sum at each index
+let arr = [1,3,5,7];const cumulativeSum = list => {
+    let result = [list[0]];
+    
+    for(let i = 1; i < list.length; i++) {
+      result.push(list[i] + result[i-1]);
+        } 
+    
+        return result;
+    }
+console.log(cumulativeSum(arr)); // Returns [1, 4, 9, 16]
